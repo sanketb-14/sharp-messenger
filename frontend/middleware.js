99b706a83522dc1119f2556e0cfe6b3c8e2c1196
sanket <sanketbarapatre999@gmail.com>
2024-07-10 16:21:09 +0530

    updating code for production

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -5,3 +5,14 @@ export const config = {
   matcher:["/account","/chats"]
 };
 
+import { NextResponse } from 'next/server'
+import squareWasm from './square.wasm?module'
+ 
+export default async function middleware() {
+  const m = await WebAssembly.instantiate(squareWasm)
+  const answer = m.exports.square(9)
+ 
+  const response = NextResponse.next()
+  response.headers.set('x-square', answer.toString())
+  return response
+}
